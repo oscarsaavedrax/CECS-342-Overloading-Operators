73d48d55c520f09e0ed979cdd0b2d84640aa093d
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-09-17 14:28:25 -0700

    Add header prompt to all files

---
+++ testupDate.cpp
@@ -0,0 +1,15 @@
+// Oscar Saavedra
+// CECS 342-07
+// Program 2 - Overloading Operators
+// September 29, 2022
+//
+// I sertify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
---
+++ upDate.cpp
@@ -0,0 +1,7 @@
+// Oscar Saavedra
+// CECS 342-07
+// Program 2 - Overloading Operators
+// September 29, 2022
+//
+// I sertify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
+
