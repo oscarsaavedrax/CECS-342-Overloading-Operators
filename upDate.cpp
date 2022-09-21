fbeb205749bbc992dae3783d7a3c775871fc4bfd
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-09-20 19:47:08 -0700

    Create default constructor

--- upDate.cpp
+++ upDate.cpp
@@ -5,3 +5,11 @@
 //
 // I sertify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
 
+#include "upDate.h"
+
+upDate::upDate()
+{
+    month = 5;
+    day = 11;
+    year = 1959;
+}
