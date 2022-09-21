7d3054b98c7a3eec3bb80aba1559ccf070c14f46
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-09-20 19:46:08 -0700

    Start test file

--- testupDate.cpp
+++ testupDate.cpp
@@ -6,10 +6,14 @@
 // I sertify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
 
 #include <iostream>
+#include "upDate.h"
 
 using namespace std;
 
 int main() {
+
+	upDate D1;
+	upDate today(9, 20, 2022);
 	
 	return 0;
 }
