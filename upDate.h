6bc7de7e694465ca9c59745e53a7630d41b6eb9d
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-09-20 19:47:41 -0700

    Add constructors and addition operator

--- upDate.h
+++ upDate.h
@@ -5,3 +5,19 @@
 //
 // I sertify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
 
+class upDate
+{
+    private:
+        int month;
+        int day;
+        int year;
+    public:
+        // Default constructor
+        upDate();
+        // Overloaded constructor
+        upDate(int, int, int);
+        // Overloaded addition operator
+        upDate operator+(int);
+
+
+};
