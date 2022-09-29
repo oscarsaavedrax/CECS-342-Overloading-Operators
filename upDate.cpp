3196f8dc6b7fd0bb9d65db5e3f5d089d6f94d914
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-09-28 17:21:51 -0700

    Fix header prompt

--- upDate.cpp
+++ upDate.cpp
@@ -3,7 +3,9 @@
 // Program 2 - Overloading Operators
 // September 29, 2022
 //
-// I sertify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
+// I certify that this program is my own original work. I did not copy any part of this 
+// program from any other source. I further certify that I typed each and every line of 
+// code in this program.
 
 #include "upDate.h"
 
